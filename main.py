9c7742b24335e26459b0353841814dce588b43b2
Kristen Sharkey <kristen.l.sharkey@gmail.com>
2017-06-29 21:05:59 -0700

    added to main, register working

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = "246Pass642"
 
 
 class Blog(db.Model):   #builds class for blog objects
@@ -23,21 +24,31 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True) #prim key to differentiate blog posts
     username = db.Column(db.String(30))
     password = db.Column(db.String(30))
-    blogs = db.relationship('Blog', backref='username' )
+    blogs = db.relationship('Blog', backref='username' ) #builds relat. between User and Blog
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    users = User.query_all()
+    usernames = users.username
+    print(usernames)
+    return render_template('index.html', usernames=usernames)
+    
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        username = request.form['username'] #get username/pass
         password = request.form['password']
-        user=User.query.filter_by(username=username).first()
-        if username and password == user.password:
-            session['username'] = username
-            return redirect('/blog')
+        user=User.query.filter_by(username=username).first() #check if username in use yet
+        if username and password == user.password: #if username in db and pass correct...
+            session['username'] = username #starts session
+            return redirect('/blog') #redirect to blog page
         elif not username:
             flash("Username not yet registered", 'error')
             return redirect('/login')
@@ -59,20 +70,35 @@ def signup():
         username=request.form['username']
         password=request.form['password']
         verify=request.form['verify']
-        current_users=User.query.filter_by(email=email).first()
-        if password != '':
+        current_users = User.query.filter_by(username = username).first()
+        if password == '':
             flash('Please enter a password', 'error')
             return redirect('/signup')
-        if username != '':
+        if username == '':
             flash('Please enter a username', 'error')
             return redirect('/signup')
         if password != verify:
             flash("Password and verify password don't match", 'error')
             return redirect('/signup')
-        if username in current_users:
-            flash("Duplicate user", 'error')
+        if len(password) < 3:
+            flash("Password must be at least 3 characters long", 'error')
             return redirect('/signup')
+        if len(username) < 3:
+            flash("Password must be at least 3 characters long", 'error')
+            return redirect('/signup')
+        if current_users != None:
+            if username in current_users:
+                flash("Duplicate user", 'error')
+                return redirect('/signup')
+        else:
+            new_user=User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/blog')
             
+    
+    return render_template('signup.html')
         
 
 
